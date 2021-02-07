361b2ff4e191145a73d0861002440831f527b4f9
xuwei <1297801812@qq.com>
2021-02-07 10:05:40 +0800

    [feature]组件化和集成化都可以正常运行

--- order/src/main/java/com/netease/modular/order/debug/Order_DebugActivity.java
+++ order/src/main/java/com/netease/modular/order/debug/Order_DebugActivity.java
@@ -1,7 +1,6 @@
 package com.netease.modular.order.debug;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.netease.common.utils.Cons;
